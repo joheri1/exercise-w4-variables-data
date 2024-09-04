37bcff5e8fc172375c9c352e1befe8217b490a79
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-09-04 13:10:19 +0200

    Added the lunch learners lab to GitHub

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ Create a variable that stores the name of your cafe.
 Print out a greeting such as "Welcome to Technigo Cafe! What would you like to order today?"
 Where Technigo Cafe is replaced by the name of your cafe that is stored in the variable.
 */
-const cafeName = "Technigo Cafe";
+const cafeName = "Lunch Learner Cafe";
 console.log(`Welcome to ${cafeName}! What would you like to order today?`);
 
 /*
@@ -14,14 +14,18 @@ Create a variable that stores how many coffees the customer wants.
 Print out the total price such as "There you go, that'll be 10 euros"
 Where 10 is replaced by the calculation of the total price.
 */
-
+const coffeePrice = 2;
+let numCoffees = 7;
+let totalPrice = coffeePrice * numCoffees;
+console.log(`There you go, that'll be ${totalPrice} euros.`);
 /*
 3)
 Create a variable that stores a boolean.
 Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
-
+let isBest = false;
+console.log(`You said this coffee is the best, that was actually ${isBest}`);
 /*
 4)
 Create a variable called cafeGuests, that shows us how many cafeGuests we have.
@@ -30,7 +34,10 @@ Print it out.
 Assign it a new value.
 Print it out. => This should give you the new value.
 */
-
+let cafeGuests = 30
+console.log(`initial ${cafeGuests}`)
+cafeGuests = 31
+console.log(`updated ${cafeGuests}`)
 /*
 5)
 Create a variable called maxGuests, that shows us how many guests are allowed in the cafe.
@@ -39,18 +46,24 @@ Print it out.
 assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
-
+const maxGuests = 50;
+console.log(`Maximal number of guests: ${maxGuests}`)
+//maxGuests = 55;
 /*
 6)
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
+let myString = "Thank you for visiting us";
+
+console.log(myString.toUpperCase());
+//
 
 /*
 7)
 Print out the same string in only lowercase letters.
 */
-
+console.log(myString.toLowerCase());
 /*
 8) **BONUS**
 Print out the string "Today we have a special summer deal!". 
@@ -58,3 +71,7 @@ Then figure out a way to replace the word "summer" in the string with the word "
 Should give you => "Today we have a special winter deal!"
 (check for a specific string method...)
 */
+let specialDeal = "Today we have a special summer deal!";
+// console.log(specialDeal);
+specialDeal = specialDeal.replace("summer", "winter");
+console.log(specialDeal);
